c47020b89a2c41116b01cde037dab50b9b1425dd
AndreeaIsabela <bala.andreea@yahoo.com>
2020-05-06 23:20:23 +0300

    [master] Change index.

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+// import { store } from './store';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './components/Main';
+
+ReactDOM.render(
+    <Main />
+    ,
+    document.getElementById('app')
+);
